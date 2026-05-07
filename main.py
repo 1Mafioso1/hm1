08363c6879e98cd2e133d919bf46b5ee367e66b7
Mafioso2304 <ilaladonenko887@gmail.com>
2026-05-07 19:40:28 +0300

    hm1

--- main.py
+++ main.py
@@ -1,174 +1,90 @@
-# Задание 1
-# Класс Student
-class Student:
-    def __init__(self, name, surname, age):
-        self.name = name
-        self.surname = surname
-        self.age = age
-        self.grades = []
-    def add_grade(self, grade):
-        self.grades.append(grade)
-    def show_grades(self):
-        print(f'Оценки студента {self.name}: {self.grades}')
-    def __str__(self):
-        return f'Имя: {self.name}\nФамилия: {self.surname}\nВозраст: {self.age}\nОценки: {self.grades}'
-
-# Класс Car
-class Car:
-    def __init__(self, brand, model, speed, year):
-        self.brand = brand
-        self.model = model
-        self.speed = speed
+class Book:
+    def __init__(self, title, authors, year) -> None:
+        self.title = title
+        self.authors = authors
         self.year = year
 
-    def show_info(self):
-        print(f'Автомобиль {self.brand} {self.model}')
-        print(f'Скорость: {self.speed} км/ч')
-        print(f'Год выпуска: {self.year}')
-
-    def __str__(self):
-        return f'Бренд: {self.brand}\nМодель: {self.model}\nСкорость: {self.speed}\nГод выпуска: {self.year}'
-
-while True:
-    print('\n===== МЕНЮ =====')
-    print('1. Создать студента')
-    print('2. Создать машину')
-    print('3. Выход')
-
-    choice = input('Выберите пункт: ')
-
-    if choice == '1':
-        name = input('Введите имя: ')
-        surname = input('Введите фамилию: ')
-        age = int(input('Введите возраст: '))
-
-        student = Student(name, surname, age)
-
-        while True:
-            grade = input('Введите оценку (или stop для завершения): ')
-
-            if grade.lower() == 'stop':
-                break
-
-            student.add_grade(int(grade))
-
-        print('\nИнформация о студенте:')
-        print(student)
-
-        student.show_grades()
-
-    elif choice == '2':
-        brand = input('Введите бренд: ')
-        model = input('Введите модель: ')
-        speed = int(input('Введите скорость: '))
-        year = int(input('Введите год выпуска: '))
-
-        car = Car(brand, model, speed, year)
-
-        print('\nИнформация об автомобиле:')
-        print(car)
-
-        car.show_info()
-
-    elif choice == '3':
-        print('Программа завершена.')
-        break
-
-    else:
-        print('Неверный пункт меню!')
-
-# Задание 2
-import math
-# Класс Circle
-
-class Circle:
-    def __init__(self, radius):
-        self.radius = radius
-
-    def area(self):
-        return math.pi * self.radius ** 2
-
-    def perimeter(self):
-        return 2 * math.pi * self.radius
-
-    def __str__(self):
-        return f'Круг: радиус = {self.radius}'
-    
-# Класс Rectangle
-
-class Rectangle:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-
-    def area(self):
-        return self.width * self.height
-
-    def perimeter(self):
-        return 2 * (self.width + self.height)
-
-    def __str__(self):
-        return f'Прямоугольник: {self.width} x {self.height}'
-
-# Класс Triangle
-
-class Triangle:
-    def __init__(self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
-
-    def area(self):
-        s = (self.a + self.b + self.c) / 2
-        return math.sqrt(s * (s - self.a) * (s - self.b) * (s - self.c))
-
-    def perimeter(self):
-        return self.a + self.b + self.c
-
-    def __str__(self):
-        return f'Треугольник: стороны {self.a}, {self.b}, {self.c}'
-
-while True:
-    print("\n===== МЕНЮ =====")
-    print("1. Круг")
-    print("2. Прямоугольник")
-    print("3. Треугольник")
-    print("4. Выход")
-
-    choice = input("Выберите фигуру: ")
-
-    if choice == "1":
-        r = float(input("Введите радиус: "))
-        circle = Circle(r)
-
-        print(circle)
-        print("Площадь:", circle.area())
-        print("Периметр:", circle.perimeter())
-
-    elif choice == "2":
-        w = float(input("Введите ширину: "))
-        h = float(input("Введите высоту: "))
-        rect = Rectangle(w, h)
-
-        print(rect)
-        print("Площадь:", rect.area())
-        print("Периметр:", rect.perimeter())
+    def __str__(self) -> str:
+        return f'Назва: {self.title}\nАвтори: {", ".join(self.authors)}\nРік: {self.year}'
 
 
-    elif choice == "3":
-        a = float(input("Сторона a: "))
-        b = float(input("Сторона b: "))
-        c = float(input("Сторона c: "))
+class Library:
+    def __init__(self, name, address) -> None:
+        self.name = name
+        self.address = address
+        self.books = []
 
-        triangle = Triangle(a, b, c)
+    def __str__(self) -> str:
+        return f'Бібліотека: {self.name}\nАдреса: {self.address}\nКількість книг: {len(self.books)}'
 
-        print(triangle)
-        print("Площадь:", triangle.area())
-        print("Периметр:", triangle.perimeter())
+    def show_books(self):
+        if len(self.books) == 0:
+            print('У бібліотеці немає книг.')
+        else:
+            print('\n=== Список книг ===')
+            for book in self.books:
+                print(book)
+                print('----------------')
 
-    elif choice == "4":
-        print("Выход...")
-        break
+    def add_book(self, book):
+        self.books.append(book)
+        print(f'Книгу "{book.title}" додано.')
 
-    else:
-        print("Неверный выбор!")
+    def remove_book(self, title):
+        for book in self.books:
+            if book.title.lower() == title.lower():
+                self.books.remove(book)
+                print(f'Книгу "{title}" видалено.')
+                return
+        print('Книгу не знайдено.')
+
+    def find_by_title(self, title):
+        found = False
+
+        for book in self.books:
+            if title.lower() in book.title.lower():
+                print(book)
+                print('----------------')
+                found = True
+
+        if not found:
+            print('Книгу не знайдено.')
+
+    def find_by_author(self, author):
+        found = False
+
+        for book in self.books:
+            for a in book.authors:
+                if author.lower() in a.lower():
+                    print(book)
+                    print('----------------')
+                    found = True
+
+        if not found:
+            print('Автора не знайдено.')
+
+library = Library('Центральна бібліотека', 'вул. Шевченка 10')
+
+book1 = Book('Кобзар', ['Тарас Шевченко'], 1840)
+book2 = Book('Майстер і Маргарита', ['Михайло Булгаков'], 1967)
+book3 = Book('Гаррі Поттер', ['Джоан Роулінг'], 1997)
+
+library.add_book(book1)
+library.add_book(book2)
+library.add_book(book3)
+
+print('\n=== Інформація про бібліотеку ===')
+print(library)
+
+library.show_books()
+
+print('\n=== Пошук за назвою ===')
+library.find_by_title('гаррі')
+
+print('\n=== Пошук за автором ===')
+library.find_by_author('Шевченко')
+
+print('\n=== Видалення книги ===')
+library.remove_book('Кобзар')
+
+library.show_books()
